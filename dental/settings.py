215f0b6ff6d10da57d21d99fd50a8b1802d5f927
waydeity <waydeity1@gmail.com>
2021-09-18 17:18:10 +0200

    static folder and dental templates

--- dental/settings.py
+++ dental/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -112,3 +113,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
